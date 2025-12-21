c54b61f8c9e62c7c9f32da75e21e321d051011e0
mokchhedulislam <mokchheduls46@gmail.com>
2025-12-21 12:48:02 +0600

    frontend store code update

--- client/src/store/auth-slice/index.js
+++ client/src/store/auth-slice/index.js
@@ -2,7 +2,7 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
 // Environment variable theke URL nawa hochche
-const API_BASE_URL = "http://ecom-server.default.svc.cluster.local:5000/api";
+const API_BASE_URL = "ad8358fbb4efc4562be9b779c8638661-1359986703.us-east-1.elb.amazonaws.com:5000/api";
 
 const initialState = {
   isAuthenticated: false,
